57f6f1c9554f70d1332f8046fc5317ca6b0e6eb2
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2023-09-11 16:03:00 +0100

    Move BASE_DIR back up to the root

--- config/settings/base.py
+++ config/settings/base.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 env = environ.Env()
 
