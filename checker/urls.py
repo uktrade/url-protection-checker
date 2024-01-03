a37fb41b982fdf4206786074de482b48954709e2
Amine Benziane <aminebenziane@DIT000126.local>
2024-01-03 13:26:01 +0000

    Upgraded django to version 4.2.8

--- checker/urls.py
+++ checker/urls.py
@@ -1,11 +1,10 @@
-from django.conf.urls import url
 
 from .views import (home_page)
 
-from django.urls import path, include
+from django.urls import path, re_path
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    url(r'^$', RedirectView.as_view(pattern_name='home_page')),
+    re_path(r'^$', RedirectView.as_view(pattern_name='home_page')),
     path('home/', home_page.as_view(), name='home_page'),
 ]
--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import include, url
+from django.urls import path, include,re_path
 from core.admin_views import admin_login_view
 
 urlpatterns = [
     path('auth/', include('authbroker_client.urls', namespace='authbroker')),
     path('admin/login/', admin_login_view, name='admin-login-view'),
     path('admin/', admin.site.urls),
-    url(r'^', include('checker.urls')),
+    re_path(r'^', include('checker.urls')),
 ]
